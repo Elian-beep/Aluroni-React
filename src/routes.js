8a69b6bffad43cdfb286c6ee0de6aafa100bc1ac
Elian-beep <elianoliver101@gmail.com>
2023-02-22 21:40:19 -0300

    not found page && footer

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
+import Footer from 'components/Footer';
 import Menu from 'components/Menu';
 import PageDefault from 'components/PageDefault';
 import About from 'pages/About';
 import Carte from 'pages/Carte';
 import Home from 'pages/Home';
+import NotFound from 'pages/NotFound';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
@@ -15,8 +17,10 @@ export default function AppRouter() {
             <Route index element={<Home />} />
             <Route path='carte' element={<Carte />} />
             <Route path='about' element={<About />} />
+            <Route path='*' element={<NotFound />} />
           </ Route>
         </Routes>
+        <Footer />
       </Router>
     </main>
   );
